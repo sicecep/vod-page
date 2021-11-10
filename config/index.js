0989ffa9f311412cd5e141345a7eaf4c8d78c691
Sepry Haryandi <sepry.haryandi@tiptip.tv>
2021-11-10 09:53:57 +0700

    add poster vod

--- config/index.js
+++ config/index.js
@@ -2,7 +2,9 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const stream_key = 'sssss'
 export const playback = 'https://stream.mux.com/gKFH4Q36tPVFbRn7KftbeY9v1JdLV24zotheyg3TI1k.m3u8'
+
 export const vod_playback = 'https://stream.mux.com/kHJ3SpO68bXY003LKssI2BFgCV3Gi5cQbipjEy18ilrA.m3u8'
+export const vod_poster = 'https://image.mux.com/kHJ3SpO68bXY003LKssI2BFgCV3Gi5cQbipjEy18ilrA/thumbnail.png?width=854&height=480&fit_mode=pad'
 
 export const server = dev ? 'http://localhost:3000' : 'https://live-stream-tip2.netlify.app';
 // https://maxbase.org
--- pages/vod.js
+++ pages/vod.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Player from '../components/Video';
-import { vod_playback } from '../config';
+import { vod_playback, vod_poster } from '../config';
 
 export default class Landing extends Component {
   render() {
@@ -11,6 +11,7 @@ export default class Landing extends Component {
       controls: true,
       width: 854,
       height: 480,
+      poster: vod_poster,
       sources: [
         {
           src: video_url,
