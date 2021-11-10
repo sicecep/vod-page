e7e96e007f92c1d8cbcb53857daef709b5a6e749
Sepry Haryandi <sepry.haryandi@tiptip.tv>
2021-11-10 21:19:57 +0700

    vod live stream

--- config/index.js
+++ config/index.js
@@ -3,7 +3,7 @@ const dev = process.env.NODE_ENV !== 'production'
 export const stream_key = 'sssss'
 export const playback = 'https://stream.mux.com/W00ksUdlwcdIcki02diYfoN2uz6ubYgnKRjUCKwYNvPpQ.m3u8'
 
-export const vod_playback = 'https://stream.mux.com/kHJ3SpO68bXY003LKssI2BFgCV3Gi5cQbipjEy18ilrA.m3u8'
+export const vod_playback = 'https://stream.mux.com/j86GaMnAaOUVRfk4d3bDoBEO4KvtQu4R1TGJyl4KAA00.m3u8'
 export const vod_poster = 'https://image.mux.com/kHJ3SpO68bXY003LKssI2BFgCV3Gi5cQbipjEy18ilrA/thumbnail.png?width=854&height=480&fit_mode=pad'
 
 export const server = dev ? 'http://localhost:3000' : 'https://live-stream-tip2.netlify.app';
